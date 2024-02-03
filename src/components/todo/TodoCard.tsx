99af7a9e5d0f0009703cbc9fd4015eeac272c342
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-03 19:47:58 +0600

    modal and filter install from shadcn

--- src/components/todo/TodoCard.tsx
+++ src/components/todo/TodoCard.tsx
@@ -1,13 +1,21 @@
+import Edit from "../icon/Edit";
+import Trash from "../icon/Trash";
+import { Button } from "../ui/button";
+
 const TodoCard = () => {
   return (
-    <div className="bg-white rounded-md flex justify-between items-center p-3">
+    <div className="bg-white rounded-md flex justify-between items-center p-3 border">
       <input type="checkbox" name="" id="" />
       <p className="font-semibold">Todo Title</p>
       <p>Time</p>
       <p>Desc</p>
       <div className="space-x-5">
-        <button>Delete</button>
-        <button>Edit</button>
+        <Button className="bg-red-500">
+          <Trash />
+        </Button>
+        <Button className="bg-[#5C53FE]">
+          <Edit />
+        </Button>
       </div>
     </div>
   );
