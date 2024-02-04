8990d1f89cf69307cf4b245c3c2e3580e6d47720
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-02-04 23:12:09 +0600

    filtering

--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,14 +1,16 @@
 import { useGetTodosQuery } from "@/redux/api/api";
+import { useState } from "react";
 import AddTodoModal from "./AddTodoModal";
 import TodoCard from "./TodoCard";
 import TodoFilter from "./TodoFilter";
 
 const TodoContainer = () => {
+  const [priority, setPriority] = useState("");
   // fetchind todos to display.
   // const { todos } = useAppSelector((state) => state.todos);
 
   // data from server by RTK query
-  const { data: todos, isLoading } = useGetTodosQuery(undefined);
+  const { data: todos, isLoading } = useGetTodosQuery(priority);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -20,7 +22,7 @@ const TodoContainer = () => {
     <div>
       <div className="flex justify-between mb-5">
         <AddTodoModal />
-        <TodoFilter />
+        <TodoFilter priority={priority} setPriority={setPriority} />
       </div>
 
       <div className="bg-primary-gradient w-full h-full rounded-lg space-y-4 p-[5px]">
